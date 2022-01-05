d44eaca7e47b8180e92e18d5b7e7d5c67348c0be
Dabizu <dabizu100@gmail.com>
2022-01-05 12:45:04 -0600

    objetos add

--- public/scenaparque.js
+++ public/scenaparque.js
@@ -19,6 +19,28 @@ fbx.load('3dmodels/chibi.fbx',function(personaje){
     
 });
 
+for ( let i = 0; i < 30; i ++ ) {
+
+    const object = new THREE.Mesh( geometry, new THREE.MeshLambertMaterial( { color: Math.random() * 0xffffff } ) );
+
+    object.position.x = Math.random() * 800 - 400;
+    object.position.y = Math.random() * 800 - 400;
+    object.position.z = Math.random() * 800 - 400;
+
+    object.rotation.x = Math.random() * 2 * Math.PI;
+    object.rotation.y = Math.random() * 2 * Math.PI;
+    object.rotation.z = Math.random() * 2 * Math.PI;
+
+    object.scale.x = Math.random() + 0.5;
+    object.scale.y = Math.random() + 0.5;
+    object.scale.z = Math.random() + 0.5;
+
+    //le asignamos un nombre
+    object.name="cubo"+i;
+
+    scene.add( object );
+
+}
 /*
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial( { color: 0x0AE1C7  } );
